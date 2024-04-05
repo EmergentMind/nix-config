9dc42d73831ce876cccfb59e9680ee761f58385e
emergentmind <2889621-emergentmind@users.noreply.gitlab.com>
2024-04-05 16:13:17 -0600

    test: tweaks for remote install

--- nixos-installer/customISO.nix
+++ nixos-installer/customISO.nix
@@ -3,8 +3,17 @@
     "${toString modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
-  #TODO: move this to flake
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  users.users.ta = {
+    isNormalUser = true;
+    password = "temp";
+    extraGroups = [ "wheel" ];
+
+    openssh.authorizedKeys.keys = [
+      (builtins.readFile ../hosts/common/users/ta/keys/id_meek.pub)
+    ];
+  };
 
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs)
--- nixos-installer/remote_install.sh
+++ nixos-installer/remote_install.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 
 target_hostname=""
 target_destination=""
+target_user="ta"
 ssh_key=""
-remote_passwd="root"
+remote_passwd="temp"
 # Create a temp directory for generated host keys
 temp=$(mktemp -d)
 
@@ -21,12 +22,15 @@ help_and_exit() {
 	echo "required:"
 	echo "  -n=<target_hostname>      specify target_hostname of the target host to deploy the nixos config on."
 	echo "  -d=<target_destination>   specify ip or url to the target host."
-	echo "  -k=<ssh_key>           specify the full path to the public ssh_key you'll use for subsequent remote access ot the target host."
-	echo "                     Example: -k=/home/ta/.ssh/my_ssh_key.pub"
+	echo "  -k=<ssh_key>       		    specify the full path to the ssh_key you'll use for remote access to the"
+	echo "														 target during install process."
+	echo "               							Example: -k=/home/ta/.ssh/my_ssh_key"
 	echo ""
 	echo "options:"
-	echo "  -p=<remote_passwd>        Specify a password for target machine root user. This is temporary until install is complete."
-	echo "                              Default='root'."
+	echo "  -u=<target_user>  				specify target_user with sudo access. nix-config will be cloned to their home."
+	echo "                              Default='ta'."
+	echo "  -p=<remote_passwd>        Specify a password for target machine user. This is temporary until install is complete."
+	echo "                              Default='temp'."
 	echo "  -h | --help               Print this help."
 	exit 0
 }
@@ -39,6 +43,9 @@ while [[ $# -gt 0 ]]; do
 		;;
 	-d=*)
 		target_destination="${1#-d=}"
+		;;
+	-u=*)
+		target_user="${1#-u=}"
 		;;
 	-k=*)
 		ssh_key="${1#-k=}"
@@ -56,97 +63,93 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Validate required options
-if [ -z "${target_hostname}" ] || [ -z "${target_destination}" ]; then
-	echo "Error: both -n and -d are required"
+if [ -z "${target_hostname}" ] || [ -z "${target_destination}" || [ -z "${ssh_key}" ]; then
+	echo "Error: -n -d and -k are required"
 	help_and_exit
 fi
 
-## Start the install process
-#echo "Installing NixOS on remote host $target_hostname at $target_destination"
+echo "Installing NixOS on remote host $target_hostname at $target_destination"
 
-#echo "Adding installer ssh host fingerprint at $target_destination to ~/.ssh/known_hosts"
-#ssh-keyscan $target_destination >>~/.ssh/known_hosts
+echo "Adding installer ssh host fingerprint at $target_destination to ~/.ssh/known_hosts"
+ssh-keyscan $target_destination >>~/.ssh/known_hosts
 
-#echo "Preparing a new ssh_host_ed25519_key pair for $target_hostname."
-## Create the directory where sshd expects to find the host keys
-#install -d -m755 "$temp/etc/ssh"
+echo "Preparing a new ssh_host_ed25519_key pair for $target_hostname."
+# Create the directory where sshd expects to find the host keys
+install -d -m755 "$temp/etc/ssh"
 
-## Generate host keys without a passphrase
-#ssh-keygen -t ed25519 -f $temp/etc/ssh/ssh_host_ed25519_key -C root@$target_hostname -N ""
+# Generate host keys without a passphrase
+ssh-keygen -t ed25519 -f $temp/etc/ssh/ssh_host_ed25519_key -C ta@$target_hostname -N ""
 
-## Set the correct permissions so sshd will accept the key
-#chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
+# Set the correct permissions so sshd will accept the key
+chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
 
-#echo "Generating an age key based on the new ssh_host_ed25519_key."
+echo "Generating an age key based on the new ssh_host_ed25519_key."
 
-#age_key=$(nix-shell -p ssh-to-age --run "cat $temp/etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age")
+age_key=$(nix-shell -p ssh-to-age --run "cat $temp/etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age")
 
-#if grep -qv '^age1' <<<"$age_key"; then
-#echo "The result from generated age key does not match the expected format."
-#echo "Result: $age_key"
-#echo "Expected format: age10000000000000000000000000000000000000000000000000000000000"
-#exit 1
-#else
-#echo "$age_key"
-#fi
+if grep -qv '^age1' <<<"$age_key"; then
+	echo "The result from generated age key does not match the expected format."
+	echo "Result: $age_key"
+	echo "Expected format: age10000000000000000000000000000000000000000000000000000000000"
+	exit 1
+else
+	echo "$age_key"
+fi
 
-#echo "Updating nix-secrets/.sops.yaml"
+echo "Updating nix-secrets/.sops.yaml"
 
-## The spacing here MUST match that of the .sops.yaml file you are formatting.
-#sops_str1="\    - &$target_hostname $age_key"
-#sops_str2="\          - *$target_hostname"
+# The spacing here MUST match that of the .sops.yaml file you are formatting.
+sops_str1="\    - &$target_hostname $age_key"
+sops_str2="\          - *$target_hostname"
 
-#sed -i "/&hosts:/ a\
-#$sops_str1" ../../nix-secrets/.sops.yaml
+sed -i "/&hosts:/ a\
+$sops_str1" ../../nix-secrets/.sops.yaml
 
-#sed -i "/age:/ a\
-#$sops_str2" ../../nix-secrets/.sops.yaml
+sed -i "/age:/ a\
+$sops_str2" ../../nix-secrets/.sops.yaml
 
-## update sopskeys
-#echo "Updating nix-secrets/.sops.yaml"
-#sops --config ../../nix-secrets/.sops.yaml updatekeys -y ../../nix-secrets/secrets.yaml
+echo "Updating nix-secrets/.sops.yaml"
+sops --config ../../nix-secrets/.sops.yaml updatekeys -y ../../nix-secrets/secrets.yaml
 
-## push the changes to nix-secrets
-#cd ../../nix-secrets
-#git commit -am "feat: added key for $target_hostname"
-#git push
-## come back to nixos-installer
-#cd -
+echo "Pushing new host key to secrets"
+cd ../../nix-secrets
+git commit -am "feat: added key for $target_hostname"
+git push
 
-## change to root nix-config directory to update flake inputs with the updated secrets
-#echo "updating flake lock on source machine with new .sops.yaml info"
-#cd ..
-#nix flake lock --update-input mysecrets
+echo "Updating flake lock on source machine with new .sops.yaml info"
+cd ../nix-config
+nix flake lock --update-input mysecrets
 
-## comeback to nixos installer
-#cd -
 ## clear the existing host entry for the target to prevent mismatch between the default keys and the new host keys we generated
-#sed -i "/$target_destination/ d" ~/.ssh/known_hosts
+cd nixos-installer
+sed -i "/$target_destination/ d" ~/.ssh/known_hosts
 
-## Execute installation
-## nixos-anywhere --extra-files "$temp" --flake '.#your-host' root@yourip
-##SHELL=/bin/sh nix run github:nix-community/nixos-anywhere --\
-##--flake .#$target_hostname \
-##root@$target_destination
-
+# Execute installation
+# nixos-anywhere --extra-files "$temp" --flake '.#your-host' root@yourip
 #SHELL=/bin/sh nix run github:nix-community/nixos-anywhere --\
-#--flake .#$target_hostname root@$target_destination
+#--flake .#$target_hostname \
+#root@$target_destination
+
+SHELL=/bin/sh nix run github:nix-community/nixos-anywhere ----flake .#$target_hostname $target_user@$target_destination
 
 echo "Adding ssh host fingerprint at $target_destination to ~/.ssh/known_hosts"
 ssh-keyscan $target_destination >>~/.ssh/known_hosts
 
 # connect to the new install and generate a hardware config based on the work declared by disko
-ssh root@$target_destination -i ~/.ssh/id_meek "nixos-generate-config"
+ssh $target_user@$target_destination -i $ssh_key "nixos-generate-config"
 
 # copy the target hardware config to nix-config on the source
-scp -i ~/.ssh/id_meek root@$target_destination:/etc/nixos/hardware-configuration.nix ../hosts/$target_hostname/hardware-configuration.nix
+scp -i $ssh_key $target_user@$target_destination:/etc/nixos/hardware-configuration.nix ../hosts/$target_hostname/hardware-configuration.nix
 
-#git commit -am "feat: add hardware config for $target_hostname"
-#git push
+git commit -am "feat: add hardware config for $target_hostname"
+git push
 
-echo "Cloning nix-config to $target_hostname"
-ssh root@$target_destination -i ~/.ssh/id_meek <<EOF
-git clone https://github.com/EmergentMind/nix-config.git
-cd nix-config
-nixos-rebuild switch --flake .#$target_hostname
-EOF
+echo "Clone nix-config on $target_hostname"
+ssh $target_user@$target_destination -i $ssh_key "git clone https://github.com/EmergentMind/nix-config.git"
+
+#TODO prune all previous generations
+
+echo ""
+echo "NixOS installed and nix-config cloned."
+echo "Sign into $target_hostname and run the following command from ~/nix-config"
+echo "sudo nixos-rebuild switch --flake .#target_hostname"
