949c25986a9ff7a8f82917b7e6abcc6e30fb6a7e
emergentmind <2889621-emergentmind@users.noreply.gitlab.com>
2024-05-04 20:44:40 -0600

    feat: hardware-configuration.nix for guppy

--- hosts/guppy/hardware-configuration.nix
+++ hosts/guppy/hardware-configuration.nix
@@ -6,7 +6,7 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
