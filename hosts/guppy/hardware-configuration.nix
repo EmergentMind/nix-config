4c9f79739db111249a51fc1d12508098c9c5fed1
emergentmind <2889621-emergentmind@users.noreply.gitlab.com>
2024-05-31 10:36:37 -0600

    feat: hardware-configuration.nix for guppy

--- hosts/guppy/hardware-configuration.nix
+++ hosts/guppy/hardware-configuration.nix
@@ -18,7 +18,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
