32215afe75009d92c0e88267805019c0179602cd
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-07-14 19:30:41 -0600

    fix: inputs not needed

--- vars/networking.nix
+++ vars/networking.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ ... }:
 { 
   #FIXME: switch this to custom port at some point. leaving it as 22 for now becuase I don't have time
   # to add all the required matchblock entries
