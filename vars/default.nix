3dc26d4dcc3086f590ba7964ce67bdd54998f8a5
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-09-12 14:58:32 -0600

    feat: add new configvars

--- vars/default.nix
+++ vars/default.nix
@@ -11,5 +11,9 @@
   gitLabEmail = "2889621-emergentmind@users.noreply.gitlab.com";
   workEmail = inputs.nix-secrets.work-email;
   persistFolder = "/persist";
+
+  # System-specific settings (FIXME: Likely make options)
   isMinimal = false; # Used to indicate nixos-installer build
+  isWork = false; # Used to indicate a host that uses work resources
+  scaling = "1"; # Used to indicate what scaling to use. Floating point number
 }
