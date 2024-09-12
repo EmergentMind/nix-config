acdc289a030c8be05f81fe71b0bab0030a98edd8
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-09-12 15:00:06 -0600

    Feat: stop using scanpaths for modules. introduced problem with set vs function

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,6 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://wiki.nixos.org/wiki/NixOS_modules).
 # These should be stuff you would like to share with others, not your personal configurations.
 
-{ configLib, ... }:
 {
-  imports = configLib.scanPaths ./.;
+  backup = import ./backup;
 }
