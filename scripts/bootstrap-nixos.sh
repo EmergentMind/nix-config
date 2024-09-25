0405eccb94655fe0f011b4a40a62c39af0afa097
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-09-25 11:23:42 -0700

    fix: workaround to stop final build failing

--- scripts/bootstrap-nixos.sh
+++ scripts/bootstrap-nixos.sh
@@ -322,8 +322,9 @@ if yes_or_no "Do you want to copy your full nix-config and nix-secrets to $targe
 	if yes_or_no "Do you want to rebuild immediately?"; then
 		green "Rebuilding nix-config on $target_hostname"
 		#FIXME there are still a gitlab fingerprint request happening during the rebuild
-		#$ssh_cmd -oForwardAgent=yes "cd nix-config && sudo nixos-rebuild --show-trace --flake .#$target_hostname" switch"
-		$ssh_cmd -oForwardAgent=yes "cd nix-config && just rebuild"
+		$ssh_cmd -oForwardAgent=yes "cd nix-config && sudo nixos-rebuild --show-trace --flake .#$target_hostname switch"
+		# FIXME: This fails because `just rebuild` tries to run `nix flake update nix-secrets` but the flake registry doesn't exist yet
+		# $ssh_cmd -oForwardAgent=yes "cd nix-config && just rebuild"
 	fi
 else
 	echo
@@ -333,7 +334,9 @@ else
 	echo "just sync $target_user $target_destination"
 	echo "To rebuild, sign into $target_hostname and run the following command from ~/nix-config"
 	echo "cd nix-config"
-	echo "just rebuild"
+	# see above FIXME
+	echo "sudo nixos-rebuild --show-trace --flake .#$target_hostname switch"
+	# echo "just rebuild"
 	echo
 fi
 
