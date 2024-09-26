b99423261dab1a7ebc9b4902011c2cbf53f5ff15
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-09-25 22:16:02 -0600

    Feat: add gamemode and gamescope. Still encountering an issue with fullscreen game windows aren't detecting mouse input correctly... tried many settings to no avail.

--- hosts/common/optional/gaming.nix
+++ hosts/common/optional/gaming.nix
@@ -1,8 +1,37 @@
 { pkgs, configVars, ... }:
 {
-  # Must be installed at the system level
-  # https://discourse.nixos.org/t/ssl-peer-certificate-or-ssh-remote-key-was-not-ok-when-launching-steam/40274
-  programs.steam.enable = true;
+  hardware.xone.enable = true; # support for the xbox controller USB dongle
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true; # Add cap_sys_nice capability to the GameScope binary so that it may renice itself.
+    };
+    # Steam must be installed at the system level
+    # https://discourse.nixos.org/t/ssl-peer-certificate-or-ssh-remote-key-was-not-ok-when-launching-steam/40274
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode = {
+      enable = true;
+      settings = {
+        #see gamemoded man page for settings info
+        general = {
+          softrealtime = "on";
+          inhibit_screensaver = 1;
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          amd_performance_level = "high";
+        };
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
+      };
+    };
+  };
   environment.variables = {
     STEAM_FORCE_DESKTOPUI_SCALING = configVars.scaling;
   };
