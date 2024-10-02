374315e3fa9207738f33ff18027af9cca1da41ec
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-10-02 11:25:51 -0600

    Refactor: remove redunancy

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -28,8 +28,6 @@ in
     flake = "${homeDirectory}/nix-config";
   };
 
-  services.yubikey-agent.enable = true;
-
   security.sudo.extraConfig = ''
     Defaults timestamp_timeout=120 # only ask for password every 2h
     # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
