8cfe08017c7c0be4e179919e8bbb89c1272f353d
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-10-03 10:11:05 -0600

    Refactor: pull in values from nix-secrets

--- vars/default.nix
+++ vars/default.nix
@@ -1,18 +1,16 @@
 { inputs, ... }:
 {
-  #TODO refactor to inherit email, domain and fullname
-  inherit (inputs.nix-secrets) networking;
+  inherit (inputs.nix-secrets)
+    userFullName
+    domain
+    email
+    networking
+    ;
 
   username = "ta";
-  domain = inputs.nix-secrets.domain;
-  userFullName = inputs.nix-secrets.full-name;
   handle = "emergentmind";
-  userEmail = inputs.nix-secrets.email.user;
   gitHubEmail = "7410928+emergentmind@users.noreply.github.com";
   gitLabEmail = "2889621-emergentmind@users.noreply.gitlab.com";
-  workEmail = inputs.nix-secrets.email.work;
-  backupEmail = inputs.nix-secrets.email.backup;
-  notifierEmail = inputs.nix-secrets.email.notifier;
   persistFolder = "/persist";
 
   # System-specific settings (FIXME: Likely make options)
@@ -20,3 +18,45 @@
   isWork = false; # Used to indicate a host that uses work resources
   scaling = "1"; # Used to indicate what scaling to use. Floating point number
 }
+
+# README
+#
+# Many of the values here come from my private nix-secrets repository.
+# While the primary purpose of nix-secrets is storing sensitive data
+# encrypted using sops, less-senstive data are simply stored in a simple
+# nix-secrets/flake.nix so they can be kept private but retrieved here
+# without the overhead of sops
+#
+# For reference the basic example structure of my nix-secrets/flake.nix is as follows:
+#
+#{
+#  outputs = {...}:
+#    {
+#        domain = "";
+#        userFullName = "";
+#        email = {
+#            user = "";
+#            work = "";
+#        };
+#        networking = {
+#            subnets = {
+#                foo = {
+#                    name = "foo";
+#                    ip = "0.0.0.0";
+#                };
+#            };
+#            external = {
+#                bar = {
+#                    name = "bar";
+#                    ip = "0.0.0.0";
+#                };
+#            };
+#            ports = {
+#                tcp = {
+#                    ssh = 22;
+#                };
+#            };
+#        };
+#
+#    };
+#}
