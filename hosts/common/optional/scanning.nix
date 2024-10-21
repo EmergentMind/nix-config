5ed70c4e4a77c40c56d52191c84736b5f35cc10c
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-10-20 21:10:29 -0600

    Feat: initial commit scanning

--- hosts/common/optional/scanning.nix
+++ hosts/common/optional/scanning.nix
@@ -3,8 +3,14 @@
   # SANE - scanner access now easy
   hardware.sane = {
     enable = true;
+    extraBackends = [
+      pkgs.samsung-unified-linux-driver
+      pkgs.hplipWithPlugin
+    ];
     #extraBackends = [ pkgs.sane-airscan ]; # For network scanners
   };
+  services.udev.packages = [ pkgs.sane-airscan ];
+  services.ipp-usb.enable = true;
 
   environment.systemPackages = builtins.attrValues {
     inherit (pkgs)
