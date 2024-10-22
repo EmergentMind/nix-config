8cb0586353240abb102b83da81405dbcb0fd8614
emergentmind <7410928+emergentmind@users.noreply.github.com>
2024-10-21 20:01:22 -0600

    feat: add udisks2 service for thunar and refactor

--- hosts/common/optional/thunar.nix
+++ hosts/common/optional/thunar.nix
@@ -1,28 +1,22 @@
 { pkgs, ... }:
-
 {
-  # xfconf is required to persist Thunar settings
-  # since we're not running on XFCE
-  programs.xfconf.enable = true;
-
-  # gvfs is for Thunar stuff like Trash folders etc
-  services.gvfs.enable = true;
-
-  # thumbnail generation service for Thunar
-  services.tumbler.enable = true;
-
-  # required for Thunar archive plugin
-  programs.file-roller.enable = true;
-
-  # Thunar
-  programs.thunar = {
-    enable = true;
-    plugins = builtins.attrValues {
-      inherit (pkgs.xfce)
-        thunar-archive-plugin
-        thunar-media-tags-plugin
-        thunar-volman
-        ;
+  programs = {
+    thunar = {
+      enable = true;
+      plugins = builtins.attrValues {
+        inherit (pkgs.xfce)
+          thunar-archive-plugin
+          thunar-media-tags-plugin
+          thunar-volman
+          ;
+      };
     };
+    xfconf.enable = true; # required to persist Thunar settings since we're not running on XFCE
+    file-roller.enable = true; # required for Thunar archive plugin
+  };
+  services = {
+    gvfs.enable = true; # for stuff like Trash folders etc
+    udisks2.enable = true; # storage device manipulation
+    tumbler.enable = true; # thumbnail generation service for Thunar
   };
 }
