a3209829afc6e13b9dacfe6ea64398307680ad35
Monaciello <88740090+Monaciello@users.noreply.github.com>
2025-02-24 15:10:51 -0500

    Update default.nix
    default shell edit to fish from zsh in hosts/common/users/(username)/default.nix

--- hosts/common/users/primary/default.nix
+++ hosts/common/users/primary/default.nix
@@ -13,7 +13,7 @@ in
 {
   users.users.${hostSpec.username} = {
     name = hostSpec.username;
-    shell = pkgs.zsh; # default shell
+    shell = pkgs.fish; # default shell
 
     # These get placed into /etc/ssh/authorized_keys.d/<name> on nixos
     openssh.authorizedKeys.keys = lib.lists.forEach pubKeys (key: builtins.readFile key);
