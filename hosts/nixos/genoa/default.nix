f574a90ed0f8f017dab2b4e1a9f62c6ffbf02869
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-03-18 19:40:01 -0600

    fix: typo

--- hosts/nixos/genoa/default.nix
+++ hosts/nixos/genoa/default.nix
@@ -17,7 +17,7 @@
     # ========== Hardware ==========
     #
 
-    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-e15-intel
+    inputs.hardware.nixosModules.lenovo-thinkpad-e15-intel
     ./hardware-configuration.nix
 
     #
