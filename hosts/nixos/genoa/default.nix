13e83d50ad866a6b733896480219cdd052a400f8
emergentmind <7410928+emergentmind@users.noreply.github.com>
2025-03-20 09:03:26 -0600

    refactor: vpn on genoa

--- hosts/nixos/genoa/default.nix
+++ hosts/nixos/genoa/default.nix
@@ -56,6 +56,7 @@
       "hosts/common/optional/nvtop.nix" # GPU monitor (not available in home-manager)
       "hosts/common/optional/obsidian.nix" # wiki
       "hosts/common/optional/plymouth.nix" # fancy boot screen
+      "hosts/common/optional/protonvpn.nix" # vpn
       "hosts/common/optional/thunar.nix" # file manager
       "hosts/common/optional/vlc.nix" # media player
       "hosts/common/optional/wayland.nix" # wayland components and pkgs not available in home-manager
